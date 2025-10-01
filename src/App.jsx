c8e0de0ec973aa237e11c61d7288a67908b54d93
mareanovazquez <mareanovazquez@protonmail.com>
2025-10-01 16:49:50 -0300

    first steps at redux

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Provider } from 'react-redux'
+import store from './store'
+
+function App() {
+
+
+  return (
+    <Provider store ={store}>
+      <div>
+        <h1>REDUX toolkit</h1>
+    /*Contador*/
+      </div>
+    </Provider>
+  )
+}
+
+export default App
---
+++ src/reducers/contadorReducer.js
@@ -0,0 +1,44 @@
+import { act } from "react"
+
+const initialState = {
+    contador: 0
+}
+
+export default function contadorReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'INCREMENTAR':
+            return {
+                ...state,
+                contador: state.contador + 1
+            }
+        case 'INCREMENTAR_5':
+            return {
+                ...state,
+                contador: state.contador + action.payload
+            }
+
+        case 'DECREMENTAR': {
+            return {
+                ...state,
+                contador: state.contador - 1
+            }
+        }
+
+        case 'DECREMENTAR_5': {
+            return {
+                ...state,
+                contador: state.contador - action.payload
+            }
+        }
+        case 'RESET': {
+            return {
+                ...state,
+                contador: 0
+            }
+        }
+
+        default:
+            return state
+
+    }
+}
